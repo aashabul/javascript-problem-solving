63e19ad943f842fb9a2ef97ff9d14e1ef6c687d5
aashabul <aashabulimam@gmail.com>
2022-04-18 01:19:57 +0600

    solved

--- sum_all_odd_fibonacci.js
+++ sum_all_odd_fibonacci.js
@@ -1,13 +1,30 @@
 /*Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num. */
 
 function sumFibs(num) {
-  let sum = 1;
-  for (let i = num; i >= 0; i--) {
-    if (i % 2 == 1) {
-      sum = sum + i;
+  if (num < 0) {
+    return "insert positive value";
+  } else if (num == 1) {
+    return 1;
+  } else {
+    let arr = [0, 1];
+    let sum = 0;
+    for (let i = 0, a = 0, b = 1; i <= num; i++) {
+      let c = a + b;
+      if (c > num) {
+        break;
+      } else {
+        a = b;
+        b = c;
+        arr.push(c);
+      }
     }
+    for (let i = 0; i <= arr.length; i++) {
+      if (arr[i] % 2 == 1) {
+        sum = sum + arr[i];
+      }
+    }
+    console.log(sum);
   }
-  return sum;
 }
-console.log(sumFibs(13));
+sumFibs(1000);
 // learning fibonacci series concepts
