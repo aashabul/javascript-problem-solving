f9811ca0ffa6196b9fe60282899bc3439c40b0aa
aashabul <aashabulimam@gmail.com>
2022-04-17 22:34:18 +0600

    learning fibonacci series

--- sum_all_odd_fibonacci.js
+++ sum_all_odd_fibonacci.js
@@ -1,8 +1,13 @@
-/*Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
-The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8. */
+/*Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num. */
 
 function sumFibs(num) {
-  return num;
+  let sum = 1;
+  for (let i = num; i >= 0; i--) {
+    if (i % 2 == 1) {
+      sum = sum + i;
+    }
+  }
+  return sum;
 }
-
-sumFibs(4);
+console.log(sumFibs(13));
+// learning fibonacci series concepts
